5f9a125943a383739d78e1696e3f9477303c8950
Bonny Castle <bonnycharade@gmail.com>
2020-02-18 21:42:20 -0800

    Header without button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import s from './Header.module.css'; 
-
+import React from "react";
+import s from "./Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+    faSearch
+   } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-    return (
+  return (
     <header className={s.header}>
-        <img src='img/logo.png' alt="tiger paw logo"/>
-        <span>about Tiger</span>
-        <span>Tiger loves</span>
-        <span>Tiger hates</span>
-        
+      <div className={s.container}>
+        <div className={s.logo}>
+          <img src="img/logo.png" alt="tiger paw logo" />
+          Super<span>Ti</span>
+        </div>
+        <div className={s.menu}>
+          <span>About</span>
+          <span>Tiger loves</span>
+          <span>Tiger hates</span>
+        </div>
+        <div className="request">
+        <FontAwesomeIcon icon={faSearch} size="sm" style={{ color: '#C0C0C0' }} />
+        </div>
+      </div>
     </header>
-    )
-}
+  );
+};
 
 export default Header;
